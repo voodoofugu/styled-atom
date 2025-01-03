a8d8d911c8e4723af715250c0afc587800297792
voodoofugu <schilin.georg@gmail.com>
2025-01-03 14:49:23 +0300

    changes for publishing

--- publish/index.d.ts
+++ publish/index.d.ts
@@ -3,7 +3,7 @@
  * @param path - Required: the style import function.
  * @example (fileName: string) => import(`../css/${fileName}.css`)
  * @returns null.
- * @see {@link https://www.npmjs.com/package/styled-atom#✦stylecore Documentation}
+ * @see {@link https://github.com/voodoofugu/styled-atom?tab=readme-ov-file#-stylecore Documentation}
  */
 
 declare const StyleCore: ({
@@ -19,12 +19,12 @@ declare const StyleCore: ({
 /**
  * `StyledAtom` component.
  * @param fileNames - Required: array of style file names.
- * @param encap - Optional: encapsulates styles using CSS file names as classes, supports custom classes.
+ * @param encap - Optional: encapsulates styles with CSS file names, supports custom classes.
  * @param fallback - Optional: React fallback element.
  * @param onLoad - Optional: callback function.
  * @param children - Optional: React children elements.
  * @returns JSX element or null.
- * @see {@link https://www.npmjs.com/package/styled-atom#styledatom Documentation}
+ * @see {@link https://github.com/voodoofugu/styled-atom?tab=readme-ov-file#-styledatom Documentation}
  */
 
 declare const StyledAtom: ({
